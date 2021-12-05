19d657372cf6d253365a3587ed6afc5602ff1519
Antonio Piedra <anto.piedrapach@hotmail.com>
2021-12-04 18:51:36 -0600

    Pagina de pago y pagina de exito de compra

--- ExamenIS/Models/ProductoModel.cs
+++ ExamenIS/Models/ProductoModel.cs
@@ -31,25 +31,63 @@ namespace ExamenIS.Models
             return Id.GetHashCode();
         }
 
-        public virtual IHtmlString GenerarResumen(int cantidad)
+        public virtual IHtmlString GenerarNombreResumen()
         {
-            String resumen = "<div class=\"row h4 m-3\" id=\"" + Id + "\">\n" +
-                             "<div class=\"col-5\">\n"+
-                             Nombre + "\n" +
-                             "</div>\n" +
-                             "<div class=\"col-2\">"+
-                             "<input type = \"number\" class=\"form-control seleccionador-cantidad\" min=\"0\" style=\"width:60%\" onchange=\"actualizarCantidad('" + Id + "','" + Precio + "')\" value=\"" + cantidad.ToString() + "\">\n"+
-                            "</div>\n"+
-                            "<div class=\"col-3 precio-total-producto\">" +
-                                "₡" + (cantidad*Precio).ToString() + "\n" +
-                            "</div>" +
-                            "<div class=\"col-1\">" +
-                                "<button type = \"button\" class=\"btn-close btn-sm\" onclick=\"eliminarProducto('" + Id + "')\"></button>" +
-                            "</div>" + 
-                           "</div>";
-      
-            IHtmlString resumenConvertido = new HtmlString(resumen);
-            return resumenConvertido;
+            String resumenNombre =  "<div class=\"col-5\">\n" +
+                                        Nombre + "\n" +
+                                    "</div>\n";
+
+            IHtmlString resumenNombreConvertido = new HtmlString(resumenNombre);
+            return resumenNombreConvertido;
+        }
+
+        public virtual IHtmlString GenerarCantidadResumen(int cantidad)
+        {
+            String resumenCantidad =  "<div class=\"col-2\">" +
+                                        "<input type = \"number\" class=\"form-control seleccionador-cantidad\" min=\"0\" style=\"width:60%\" onchange=\"actualizarCantidad('" + Id + "','" + Precio + "')\" value=\"" + cantidad.ToString() + "\">\n" +
+                                    "</div>\n";
+
+            IHtmlString resumenCantidadConvertido = new HtmlString(resumenCantidad);
+            return resumenCantidadConvertido;
+        }
+
+        public virtual IHtmlString GenerarCantidadResumenPago(int cantidad)
+        {
+            String resumenCantidad = "<div class=\"col-2\">" +
+                                        cantidad.ToString() +"\n"+
+                                    "</div>\n";
+
+            IHtmlString resumenCantidadConvertido = new HtmlString(resumenCantidad);
+            return resumenCantidadConvertido;
+        }
+
+        public virtual IHtmlString GenerarBotonEliminarResumen()
+        {
+            String resumenPrecio =  "<div class=\"col-1\">" +
+                                        "<button type = \"button\" class=\"btn-close btn-sm\" onclick=\"eliminarProducto('" + Id + "')\"></button>" +
+                                    "</div>";
+
+            IHtmlString resumenPrecioConvertido = new HtmlString(resumenPrecio);
+            return resumenPrecioConvertido;
+        }
+        public virtual IHtmlString GenerarPrecioResumen(int cantidad)
+        {
+            String resumenPrecio = "<div class=\"col-3 precio-total-producto\">" +
+                                        "₡" + (cantidad * Precio).ToString() + "\n" +
+                                    "</div>";
+
+            IHtmlString resumenPrecioConvertido = new HtmlString(resumenPrecio);
+            return resumenPrecioConvertido;
+        }
+
+        public virtual IHtmlString GenerarDetalles() {
+            String resumenDetalles = "";
+            IHtmlString resumenDetallesConvertido = new HtmlString(resumenDetalles);
+            return resumenDetallesConvertido;
+        }
+
+        public virtual String GenerarMargenes() {
+            return "m-3";
         }
     }
 }
